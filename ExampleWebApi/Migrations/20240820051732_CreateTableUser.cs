d4ddbdc000a9be8e9122fab1372a2fd529615356
raulsfreitas1997@hotmail.com <raulsfreitas1997@hotmail.com>
2024-08-20 02:23:44 -0300

    Issue #2: First Migration: Create Table user

---
+++ ExampleWebApi/Migrations/20240820051732_CreateTableUser.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ExampleWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    user_password = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.user_id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_user_name",
+                table: "users",
+                column: "user_name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
